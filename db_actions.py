21dc9ee20ca8e1282d422e190173ca7629d1b2ea
Max van Steen <max.vansteen@live.nl>
2019-11-07 13:45:47 +0000

    insert user and insert profile for db_actions

--- db_actions.py
+++ db_actions.py
@@ -7,3 +7,31 @@ db = TinyDB(DB_NAME)
  
 def get_db():
     return db.all()
+ 
+def insert_user(user_data):
+    """
+    Insert user data
+    :param user_id:
+    :param profile_data:
+    :return:
+    """    
+    result = user_data
+    user_data['type'] = 'user'
+    for profile in user_data['profile']:
+        insert_profile(profile)
+        
+    user_data.pop('profile')
+    db.insert(user_data)
+    return result
+
+def insert_profile(profile):
+    """
+    Insert profile data into table
+    :param user_id:
+    :param profile_data:
+    :return:
+    """
+    fields = []
+    fields.append({'type': 'profile', 'id': str(uuid.uuid4()),'user_id': profile.get('user_id', None), 'field': profile.get('field', None), 'value': profile.get('value', None)})
+    db.insert_multiple(fields)
+    return profile
